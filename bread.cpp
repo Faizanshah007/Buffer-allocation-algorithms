7182f0de32702aae3ebaa91bf8894d7280130463
Faizanshah007 <faizanshah.ansari@gmail.com>
2018-10-16 00:02:12 +0530

    Update bread.cpp
    The basic outline of bread function.

--- bread.cpp
+++ bread.cpp
@@ -1,1 +1,26 @@
+boolean disk_read_complete = True;
 
+boolean validate ( buffer b )
+{
+    
+}
+
+void dsk_read()
+{
+    
+}
+
+buffer bread ( int fs_no, int blk_no )
+{
+    buff = getblk(fs_no, blk_no);
+    
+    if ( validate( buff ) )
+      return buff;
+  
+    dsk_read();
+    
+    while( !disk_read_complete )
+      sleep();
+      
+    return buff;
+}
